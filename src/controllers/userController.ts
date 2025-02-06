83a7e573006ba556e80675a66d79af9888e53216
Rahul <kumarrahul9977@gmail.com>
2025-02-06 11:08:18 +0530

    Add user login and register functionality

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,30 +1,120 @@
-import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import {User} from '../models/UserModel'; // Adjust based on your project structure
+import { Request, Response } from "express";
+import { User } from "../models/UserModel";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-export const registerUser = async (req: Request, res: Response): Promise<any> => {
-  const { username, password } = req.body;
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const { username, password, email, confirmPassword } = req.body;
+  if (
+    username === "" ||
+    email === "" ||
+    password === "" ||
+    confirmPassword === ""
+  ) {
+    return res.status(200).json({
+      result: false,
+      message:
+        "requied parameters : username , email , password, confirmPassword",
+    });
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      result: false,
+      message: "password not match",
+    });
+  }
 
   try {
-    // Check if the user already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username already exists' });
+    const user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(400).json({
+        result: false,
+        message: "user already exist",
+      });
     }
 
-    // Create a new user
-    const user = new User({ username, password });
-    await user.save();
+    if (confirmPassword === password) {
+      const hasPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username: username,
+        email: email,
+        password: hasPassword,
+      });
 
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
-      expiresIn: '1h',
+      const data = await newUser.save();
+      return res.status(200).json({
+        result: true,
+        message: "User signup successfully",
+        data: data,
+      });
+    }
+
+    return res.status(400).json({
+      result: false,
+      message: "Signup failed",
     });
-
-    // Send the response
-    return res.status(201).json({ message: 'User registered successfully', token });
-  } catch (error) {
-    console.error('Error registering user:', error);
-    return res.status(500).json({ message: 'Internal server error' });
+  } catch (error: any) {
+    res.status(500).json({
+      result: false,
+      message: error.message,
+    });
   }
 };
+
+
+export const loginUser = async (req: Request, res: Response): Promise<any> => {
+  const { email, password } = req.body;
+  if (email === null && password === null) {
+    return res.status(200).json({
+      result: false,
+      message: "requied fillerd : email, password",
+    });
+  }
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({
+        result: false,
+        message: "user not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(404).json({
+        result: false,
+        message: "password not match",
+      });
+    }
+
+    const payload = {
+      id: user._id,
+      name: user.username,
+    };
+
+    const secret_key = process.env.JWT_SECRET_KEY;
+
+    const token = jwt.sign(payload, secret_key, { expiresIn: "1d" });
+
+    res.header("Authorization", `Bearer ${token}`);
+    return res.status(200).json({
+      result: true,
+      message: "user login successfully",
+      data: {
+        _id: user._id,
+        token: token,
+      },
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      result: false,
+      message: error.message,
+    });
+  }
+};
+
+
